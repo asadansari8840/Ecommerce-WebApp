0be1a77471a50c957ace4f4cb3af2814ab24cb3c
asadansari8840 <asadansari8840@gmail.com>
2023-03-03 20:25:40 +0530

    Ecommerce

---
+++ index.js
@@ -0,0 +1,46 @@
+const app = require("./app");
+const dotenv = require("dotenv");
+const connectDatabase = require("./config/database");
+const cloudinary = require("cloudinary");
+const path = require("path");
+const express = require("express");
+//Handling uncaught exception 
+process.on("uncaughtException", (err) => {
+    console.log(`Error: ${err.message}`);
+    console.log(`Shutting down the server due to uncaughtException`);
+    process.exit(1);
+})
+
+//config 
+dotenv.config({ path: "./config/config.env" });
+//Connecting to database
+connectDatabase();
+
+cloudinary.config({
+    cloud_name : process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret : process.env.CLOUDINARY_API_SECRET,
+})
+
+
+const server = app.listen(process.env.PORT, () => {
+    console.log(`Server is working on http://localhost:${process.env.PORT}`)
+})
+
+//* Serve static assets in production, must be at this location of this file
+if (process.env.NODE_ENV === 'production') {
+    //*Set static folder
+    app.use(express.static('client/build'));
+    
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+  }
+
+
+//Unhandled Promise Rejection of mongo db 
+process.on("unhandledRejection", err => {
+    console.log(`Error: ${err.message}`);
+    console.log(`Shutting the server due to unhandled promise rejection : ` + err);    
+    server.close(() => {
+        process.exit(1);
+    })
+})
